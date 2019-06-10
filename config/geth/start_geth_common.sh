2f3d180023ad13fc5cde84538552de318298f2d2
wxymac <609402897@qq.com>
2019-06-10 14:35:06 +0800

    set difficulty 0xfffff

---
+++ config/geth/start_geth_common.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+geth --datadir ./ init ./work/genesis.json
+geth --datadir ./ --networkid 88 --rpcport 8545 --rpc --rpcaddr 0.0.0.0 --rpcapi db,eth,net,web3,personal,admin,miner --bootnodes enode://b2d4eab8033a590a49d4deff7ff1c6f502946db7c995a9da383e76588b16301a680ab5f802ad9a1c2c830856c0ee5a691800ea19dc3539842293744bf64588f0@172.19.0.254:30301
