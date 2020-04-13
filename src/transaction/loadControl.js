59a0f222b35f65de5ff342d0efca34cf046ce16b
wxymac <609402897@qq.com>
2020-04-14 00:21:14 +0800

    new version with failure injection

--- src/transaction/loadControl.js
+++ src/transaction/loadControl.js
@@ -7,10 +7,8 @@ const winston = require('winston');
 function startLoadTimer(loadConfig) {
     let period = loadConfig.period;
     let doubleTimes = loadConfig.doubleTimes;
-    let index = 2;
     for (let i = 1; i < doubleTimes; i++) {
-        setTimeout(clientControl.controlRateIndex, i * period * 1000, index);
-        index *= 2;
+        setTimeout(clientControl.controlRateIndex, i * period * 1000, i + 1);
     }
 }
 
